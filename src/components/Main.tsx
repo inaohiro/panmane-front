92b06e5008c284f00a72f8cf158eccbef5594c5a
keikorin <41621059+keikorin@users.noreply.github.com>
2018-07-25 11:28:22 +0900

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -21,6 +21,21 @@ const Main = ({ current }: Props) => (
       </div>
     </div>
   </div>
+  
+  <div className="main">
+    <div className="graph">
+      <div idName="progressive-circle01" className="chart pantsmater"></div>
+      <div idName="progressive-circle02" className="chart"></div>
+      <p className="min">min</p><p class="max">max</p>
+    </div>
+    <div className="main-bottom">
+      <img src="images/push.png" idName="wash" alt="洗濯完了！" className="washed">
+      <img src="images/senzai.png" alt="洗剤" className="senzai">
+      <img src="images/setting.png" alt="設定" className="setting">
+    </div>
+  </div>
+  
+  
 );
 
 export default Main;
