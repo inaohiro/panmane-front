4d1567e381d58dae06b57cee5ae54b91feed5b7e
keikorin <41621059+keikorin@users.noreply.github.com>
2018-07-25 11:32:11 +0900

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,14 +8,15 @@ interface Props {
 const Main = ({ current }: Props) => (
   
   
+  
   <div className="main">
     <div className="graph">
-      <div idName="progressive-circle01" className="chart pantsmater"></div>
-      <div idName="progressive-circle02" className="chart"></div>
+      <div id="progressive-circle01" className="chart pantsmater"></div>
+      <div id="progressive-circle02" className="chart"></div>
       <p className="min">min</p><p class="max">max</p>
     </div>
     <div className="main-bottom">
-      <img src="images/push.png" idName="wash" alt="洗濯完了！" className="washed">
+      <img src="images/push.png" id="wash" alt="洗濯完了！" className="washed">
       <img src="images/senzai.png" alt="洗剤" className="senzai">
       <img src="images/setting.png" alt="設定" className="setting">
     </div>
