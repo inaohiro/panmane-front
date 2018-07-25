fe2bcdaa01e0b95e31cccb8115f2ca85e8e26abb
keikorin <41621059+keikorin@users.noreply.github.com>
2018-07-25 15:07:42 +0900

    Update Settings.tsx

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -90,18 +90,44 @@ class Settings extends React.Component<Props, PropsState> {
           </div>
         </li>
       </ul>
+      <img src="images/step2.png" class="stepimg">
+      <p>あなたが洗濯物を干す地域を選択してください。</p>
+      <div className="cp_ipselect2 cp_sl02">
+        <select required>
+          <option value="" hidden>市町村</option>
+          <option value="adachi">足立区</option>
+          <option value="arakawa">荒川区</option>
+          <option value="itabashi">板橋区</option>
+          <option value="edogawa">江戸川区</option>
+          <option value="oota">大田区</option>
+          <option value="kasai">葛西区</option>
+          <option value="kita">北区</option>
+          <option value="etou">江東区</option>
+          <option value="sinagawa">品川区</option>
+          <option value="shibuya">渋谷区</option>
+          <option value="shinjyuku">新宿区</option>
+          <option value="suginami">杉並区</option>
+          <option value="sumida">墨田区</option>
+          <option value="setagaya">世田谷区</option>
+          <option value="daitou">台東区</option>
+          <option value="tyuuou">中央区</option>
+          <option value="chiyoda">千代田区</option>
+          <option value="toyosima">豊島区</option>
+          <option value="nakano">中野区</option>
+          <option value="nerima">練馬区</option>
+          <option value="bunkyo">文京区</option>
+          <option value="minato">港区</option>
+          <option value="meguro">目黒区</option>
+        </select>
+      </div>
     </div>
     </div>
-        <Link to="/">
-          <button
-            className="btn"
-            type="button"
-            onClick={this.handleClick}
-            disabled={this.state.max < this.state.current}
-          >
-            OK
-          </button>
-        </Link>
+      <Link to="/">
+        <div className="next"><span onClick={this.handleClick} className="ok">OK</span></div>
+      </Link>
+        
+          
+        
         <Route path="/" compknent={App} />
       </>
     );
