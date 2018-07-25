e1ae6df952949051973cf9dc78519a4faec211bf
keikorin <41621059+keikorin@users.noreply.github.com>
2018-07-25 17:00:44 +0900

    Update Weather.tsx

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -9,12 +9,22 @@ interface Props {
 
 const Weather = ({ count, data, index }: Props) => {
   const _count = count < 0 ? 0 : count;
+  
+  var bgcolor;
+  if(data.rainprobability < 21){
+    bgcolor = "orange";
+    }elseif(data.rainprobability < 41){
+      bgcolor = "yellow";
+    }elseif(data.rainprobability < 61){
+      bgcolor = "green";
+    }else{
+      bgcolor = "blue";
+    }
+  }
 
   return (
     <li
-      className={
-        index % 3 === 0 ? "orange" : index % 3 === 1 ? "yellow" : "blue"
-      }
+      className={bgcolor};
     >
       <div className="week-left">
         <p>
@@ -27,18 +37,18 @@ const Weather = ({ count, data, index }: Props) => {
         <p className="pants-will">Pants Count：{_count}</p>
         <br />
         {data.weatherAM9 === "晴れ" ? (
-          <img src="images/tenki-icon-sunny.png" alt="am-wether" />
+          <img src="images/tenki-icon-sunny.png" />
         ) : (
-          <img src="images/tenki-icon-rain.png" alt="am-wether" />
+          <img src="images/tenki-icon-rain.png" />
         )}
         <p>
           <span>{data.rainprobabilityAM9}</span>%
         </p>
-        /
+        <p className="slash">/</p>
         {data.weatherPM3 === "晴れ" ? (
-          <img src="images/tenki-icon-sunny.png" alt="am-wether" />
+          <img src="images/tenki-icon-sunny.png" />
         ) : (
-          <img src="images/tenki-icon-rain.png" alt="am-wether" />
+          <img src="images/tenki-icon-rain.png" />
         )}
         <p>
           <span>{data.rainprobabilityPM3}</span>%
