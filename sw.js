0dc52219ada06284a79ac17898536d8e76208e6f
Naohiro Iwamoto <naohiro-iwamoto@gmo.jp>
2018-07-26 05:27:19 +0900

    getToken is under registration of request permission

--- sw.js
+++ sw.js
@@ -7,6 +7,8 @@ firebase.initializeApp({
   'messagingSenderId': '958465683662'
 });
 
+const messaging = firebase.messaging();
+
 self.addEventListener("push", function (event) {
   event.waitUntil(
     self.registration.showNotification(event.data.json().notification.title, {
@@ -16,7 +18,23 @@ self.addEventListener("push", function (event) {
   );
 });
 
+// if the app running in foreground 
 messaging.onMessage(function (payload) {
   console.log('Message received. ', payload);
   // ...
+});
+
+
+// if the app runninng in background
+messaging.setBackgroundMessageHandler(function (payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  var notificationTitle = 'My Push Notification!!!';
+  var notificationOptions = {
+    body: 'Message body.',
+    icon: '/images/icons-192.png'
+  };
+
+  return self.registration.showNotification(notificationTitle,
+    notificationOptions);
 });
