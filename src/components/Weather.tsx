7fa8b3ae0926a0fb16620045aa5b720d1d46fc30
Naohiro Iwamoto <naohiro-iwamoto@gmo.jp>
2018-07-25 19:34:35 +0900

    avoid access object of undefined

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -10,12 +10,13 @@ const Weather = ({ count, data }: Props) => {
   const _count = count < 0 ? 0 : count;
 
   var bgcolor;
-  console.log("data: ", data)
-  if (Math.max(data.rainprobabilityAM9, data.rainprobabilityPM3) < 21) {
+  const rainp_am9 = data && data.rainprobabilityAM9 || 0;
+  const rainp_pm3 = data && data.rainprobabilityPM3 || 0;
+  if (Math.max(rainp_am9, rainp_pm3) < 21) {
     bgcolor = "orange";
-  } else if (Math.max(data.rainprobabilityAM9, data.rainprobabilityPM3) < 41) {
+  } else if (Math.max(rainp_am9, rainp_pm3) < 41) {
     bgcolor = "yellow";
-  } else if (Math.max(data.rainprobabilityAM9, data.rainprobabilityPM3) < 61) {
+  } else if (Math.max(rainp_am9, rainp_pm3) < 61) {
     bgcolor = "green";
   } else {
     bgcolor = "blue";
@@ -27,30 +28,30 @@ const Weather = ({ count, data }: Props) => {
     >
       <div className="week-left">
         <p>
-          {data.date}
+          {data && data.date || ""}
           <br />
-          {data.day_of_the_week}
+          {data && data.day_of_the_week || ""}
         </p>
       </div>
       <div className="week-right">
         <p className="pants-will">この日のパンツ枚数：{_count}</p>
         <br />
-        {data.weatherAM9 === "晴れ" ? (
+        {(data && data.weatherAM9 || "") === "晴れ" ? (
           <img src="images/tenki-icon-sunny.png" />
         ) : (
             <img src="images/tenki-icon-rain.png" />
           )}
         <p>
-          <span>{data.rainprobabilityAM9}</span>%
+          <span>{data && data.rainprobabilityAM9 || ""}</span>%
         </p>
         <p className="slash">/</p>
-        {data.weatherPM3 === "晴れ" ? (
+        {(data && data.weatherPM3 || "") === "晴れ" ? (
           <img src="images/tenki-icon-sunny.png" />
         ) : (
             <img src="images/tenki-icon-rain.png" />
           )}
         <p>
-          <span>{data.rainprobabilityPM3}</span>%
+          <span>{data && data.rainprobabilityPM3 || ""}</span>%
         </p>
       </div>
     </li>
