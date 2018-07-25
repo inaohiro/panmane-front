c8181ef7c1ff67a116e6a7d5f267220e434c18a3
Naohiro Iwamoto <naohiro-iwamoto@gmo.jp>
2018-07-25 20:43:38 +0900

    delete unused character ">"

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -27,11 +27,11 @@ class Main extends React.Component<Props, {}> {
             <p className="pantscount-title">Pants Count</p>
             <p className="pantscount">{this.props.pants.current}</p>
             <div className="main-bottom">
-              <img src="images/push.png" alt="洗濯完了！" className="washed" />>
-      <img src="images/senzai.png" alt="洗剤" className="senzai" />>
-      <Link to="/settings">
-                <img src="images/setting.png" alt="設定" className="setting" />>
-      </Link>
+              <img src="images/push.png" alt="洗濯完了！" className="washed" />
+              <img src="images/senzai.png" alt="洗剤" className="senzai" />
+              <Link to="/settings">
+                <img src="images/setting.png" alt="設定" className="setting" />
+              </Link>
             </div>
           </div>
         </div>
