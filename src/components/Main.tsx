91cec33b920d900eba1e3a8e7ed74e57f408c842
keikorin <41621059+keikorin@users.noreply.github.com>
2018-07-25 11:30:13 +0900

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,21 +6,7 @@ interface Props {
 }
 
 const Main = ({ current }: Props) => (
-  <div className="main">
-    <div className="maincircle">
-      <div className="pantscircle">
-        <p className="pantscount-title">Pants Count</p>
-        <p className="pantscount">{current}</p>
-        <div className="main-bottom">
-          <img src="images/push.png" alt="洗濯完了！" className="washed" />>
-          <img src="images/senzai.png" alt="洗剤" className="senzai" />>
-          <Link to="/settings">
-            <img src="images/setting.png" alt="設定" className="setting" />>
-          </Link>
-        </div>
-      </div>
-    </div>
-  </div>
+  
   
   <div className="main">
     <div className="graph">
