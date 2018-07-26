474807c228df84f6c80f2ff4e789209abedc995e
keikorin <41621059+keikorin@users.noreply.github.com>
2018-07-26 13:11:53 +0900

    Update Tutorial.tsx

--- src/components/Tutorial.tsx
+++ src/components/Tutorial.tsx
@@ -7,7 +7,7 @@ const Tutorial = () => (
   <>
     
     <video id="video">
-      <source src="images/tutrial_all.mp4" type="video/mp4"/>
+      <source src="images/tutrial_all.mp4" type="video/mp4" autoplay loop />
       <p>video要素がサポートされていないブラウザでご覧になっています。</p>
     </video>
     <Link to="/settings">
