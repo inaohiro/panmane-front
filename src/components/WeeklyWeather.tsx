8b32969df619a13b4d3ea8e8887dd4c82fec5055
Naohiro Iwamoto <naohiro-iwamoto@gmo.jp>
2018-07-26 10:30:47 +0900

    地点名を天気データについている部分から取得

--- src/components/WeeklyWeather.tsx
+++ src/components/WeeklyWeather.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const WeeklyWeather = ({ location_ja, count, weather }: Props) => (
   <div className="weekly-weather">
-    <p className="weekly-weather-title">{location_ja} の１週間の天気</p>
+    <p className="weekly-weather-title">{weather.slice(-1)} の１週間の天気</p>
     <div className="week-box">
       {weather.slice(0, weather.length - 1).map((w, index) => (
         // count should be decrease one by one
